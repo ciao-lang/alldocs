507ebb1d9a39f01cbc369dc9db2e5d7f7f47a20d
Jose F. Morales <jfmcjf@gmail.com>
2017-01-09 18:29:28 +0100

    Public snapshot for 7f4433210b816bb8b228c831ce6d7e1cf542e276
    Src-commit: 7f4433210b816bb8b228c831ce6d7e1cf542e276

--- reference/SETTINGS.pl
+++ reference/SETTINGS.pl
@@ -261,25 +261,6 @@ docstr_interfaces :=
 	 %
 	 'davinci/davinci',
 	 %
-	 'tcltk/tcltk'-['tcltk/tcltk_low_level'],
-	 %
-%  'window_class_doc',
-%    'widget_class_doc',
-%      'menu_class_doc',
-%      'canvas_class_doc',
-%      'button_class_doc',
-%      'checkbutton_class_doc',
-%      'radiobutton_class_doc',
-%      'entry_class_doc',
-%      'label_class_doc',
-%      'menubutton_class_doc',
-%      'menu_entry_class_doc',
-%    'shape_class_doc',
-%      'arc_class_doc',
-%      'oval_class_doc',
-%      'poly_class_doc',
-%      'line_class_doc',
-%      'text_class_doc',
 	 'pillow/pillow_doc'-[
 	   'pillow/html',
 	   'pillow/http',
@@ -338,25 +319,6 @@ docstr_adts :=
 %     'keys',
 %     'llists',
 %     'lsets'
-
-% (chartlib moved to library.development/)
-% 'chartlib/chartlib'-[
-%   'chartlib/bltclass',
-%   'chartlib/chartlib_errhandle',
-%   'chartlib/color_pattern',
-%   'chartlib/genbar1',
-%   'chartlib/genbar2',
-%   'chartlib/genbar3',
-%   'chartlib/genbar4',
-%   'chartlib/gengraph1',
-%   'chartlib/gengraph2',
-%   'chartlib/genmultibar',
-%   'chartlib/table_widget1',
-%   'chartlib/table_widget2',
-%   'chartlib/table_widget3',
-%   'chartlib/table_widget4',
-%   'chartlib/test_format'
-% ],
 	 
 docstr_contrib :=
 	['ddlist/ddlist',
@@ -391,10 +353,6 @@ docstr_contrib :=
 	 'regexp/regexp_doc'-['regexp/regexp_code'],
 	 %
 	 'tester/tester'].
-
-% (xml_extra bundle)
-% 'xdr_handle/xdr_handle',
-% 'xml_path/doc/xml_path_doc'
 
 docstr_utilscontrib :=
 	['cleandirs'].

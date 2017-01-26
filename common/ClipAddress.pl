5b94e1649338efb2b06d965f32508ffbc3586cd0
Jose F. Morales <jfmcjf@gmail.com>
2017-01-26 16:20:40 +0100

    Public snapshot for 988373a921c76ffaa109ed7af58fa7cec88d7a57
    Src-commit: 988373a921c76ffaa109ed7af58fa7cec88d7a57

--- common/ClipAddress.pl
+++ common/ClipAddress.pl
@@ -6,7 +6,7 @@
 :- doc(address, "The Computational logic, Languages, ").
 :- doc(address, "Implementation, and Parallelism (CLIP) Lab").
 %% :- doc(address, "The CLIP Group").
-:- doc(address, "@href{http://www.cliplab.org/}").
+:- doc(address, "@href{https://www.cliplab.org/}").
 :- doc(address, "@email{webmaster@@clip.dia.fi.upm.es}").
 %:- doc(address, "School of CS, Technical University of Madrid"). 
 :- doc(address, "School of CS, T. U. of Madrid (UPM)"). 

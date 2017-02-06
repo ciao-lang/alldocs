83d9a8413fe02c4c3ab03f1cb072bec73e1aa39b
Jose F. Morales <jfmcjf@gmail.com>
2017-02-06 15:04:54 +0100

    Public snapshot for 68fbc14a8591e598b7f676fb9bb6d18d141db0e4
    Src-commit: 68fbc14a8591e598b7f676fb9bb6d18d141db0e4

--- common/LPDOCCOMMON.pl
+++ common/LPDOCCOMMON.pl
@@ -4,5 +4,4 @@
 ciaofilepath_common := at_bundle(alldocs, 'common').
 ciaofilepath_common := at_bundle(alldocs, 'bibtex').
 
-ciao_bibfile := 'clip.bib'.
-ciao_bibfile := 'general.bib'.
+ciao_bibfile := 'common.bib'.

b9f8c6a4e7a53790375f0f42c41a2fd3e5d778e2
Jose F. Morales <jfmcjf@gmail.com>
2017-03-07 07:58:54 +0100

    Public snapshot for 37d88d84f8afa09b0c13a6d2e14b0d9c6adb4364
    Src-commit: 37d88d84f8afa09b0c13a6d2e14b0d9c6adb4364

--- Manifest/Manifest.pl
+++ Manifest/Manifest.pl
@@ -1,7 +1,7 @@
 :- bundle(alldocs).
 % General Ciao documentation
 % (manuals and tutorials covering main bundles)
-version('1.16.0-alpha.2').
+version('1.16.0-alpha.3').
 depends([
   core,
   lpdoc,

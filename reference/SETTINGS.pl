68dcc01779e44a3a46bdb02ad0a076b1055e0a0e
Jose F. Morales <jfmcjf@gmail.com>
2018-08-07 20:00:04 +0200

    (alldocs) Minor change in docstr predicates
    ... so that summary is next to the sections for each part.
    
    Src-commit: 9b06b051542014a17f5a3ad20125c18a03976f92

--- reference/SETTINGS.pl
+++ reference/SETTINGS.pl
@@ -17,70 +17,75 @@ filepath := at_bundle(ciao_emacs, 'elisp').
 
 doc_structure := 
         ciao-[
-	  'GettingStarted'-(~docstr_gettingstarted),
-	  'DevEnv'-(~docstr_devenv),
-	  'BasicLang'-(~docstr_basiclang),
-	  'AssrtLang'-(~docstr_assrtlang),
-	  'ExtendLang'-(~docstr_extendlang),
-	  'CompatProlog'-(~docstr_compatprolog),
-	  'DataStructuresAlgs'-(~docstr_datastructuresalgs),
-	  'StdLibs'-(~docstr_stdlibs),
-	  'ExtraLibs'-(~docstr_extralibs)
+	  ~docstr_gettingstarted,
+	  ~docstr_devenv,
+	  ~docstr_basiclang,
+	  ~docstr_assrtlang,
+	  ~docstr_extendlang,
+	  ~docstr_compatprolog,
+	  ~docstr_datastructuresalgs,
+	  ~docstr_stdlibs,
+	  ~docstr_extralibs
         ].
 
 % ---------------------------------------------------------------------------
 
 docstr_gettingstarted :=
-	['Install',
-	 'InstallWin32bin',
-	 'CommandLineUse',
-	 'EmacsUse'
-%	 'BeyondInstall' % TODO: move to website
-	 ].
+	'GettingStarted'-[
+          'Install',
+	  'InstallWin32bin',
+	  'CommandLineUse',
+	  'EmacsUse'
+%	  'BeyondInstall' % TODO: move to website
+	].
 
 % ---------------------------------------------------------------------------
 
 docstr_devenv :=
-	['toplevel/toplevel_doc',
-	 'debugger/debugger_doc'-['debugger/debugger'],
-	 'builder_doc',
-	 'ciaoc',
-	 'ciao-shell',
-	 'ciao-utilities'-(~docstr_utils),
-	 'CiaoMode'].
+	'DevEnv'-[
+          'toplevel/toplevel_doc',
+	  'debugger/debugger_doc'-['debugger/debugger'],
+	  'builder_doc',
+	  'ciaoc',
+	  'ciao-shell',
+	  ~docstr_utils,
+	  'CiaoMode'
+        ].
 
 % (those are part of the development environment)
 docstr_utils :=
-	['ciaodump',
-%        'xrefs_doc',
-	 'xrefs/callgraph',
-	 % 'show_deps'?
-%	 'get_deps',
-	 'pldiff'].
+	'ciao-utilities'-[
+          'ciaodump',
+	  % 'xrefs_doc',
+	  'xrefs/callgraph',
+	  % 'show_deps'?
+	  % 'get_deps',
+	  'pldiff'
+	].
 
 % ---------------------------------------------------------------------------
 
 docstr_basiclang :=
-	[% (Code organization)
-	 'modules', % engine
-	 'bundles_doc', % engine
-	 'packages', % engine
-	 'condcomp/condcomp_doc',
-	 % (Predicates and terms)
-	 'basiccontrol', % engine
-	 'exceptions', % engine
-	 'basic_props', % engine
-	 'term_typing', % engine
-	 'term_basic', % engine
-	 'term_compare', % engine
-	 'atomic_basic', % engine
-	 'arithmetic', % engine
-	 % (Dynamic database)
-	 'data_facts', % engine
-	 'dynamic', % (OPTIONAL)
-	 'dynamic_clauses/dynamic_clauses_doc'-[ % (OPTIONAL)
-	   'dynamic_clauses/dynamic_clauses_rt'
- 	 ]
+	'BasicLang'-[% (Code organization)
+	  'modules', % engine
+	  'bundles_doc', % engine
+	  'packages', % engine
+	  'condcomp/condcomp_doc',
+	  % (Predicates and terms)
+	  'basiccontrol', % engine
+	  'exceptions', % engine
+	  'basic_props', % engine
+	  'term_typing', % engine
+	  'term_basic', % engine
+	  'term_compare', % engine
+	  'atomic_basic', % engine
+	  'arithmetic', % engine
+	  % (Dynamic database)
+	  'data_facts', % engine
+	  'dynamic', % (OPTIONAL)
+	  'dynamic_clauses/dynamic_clauses_doc'-[ % (OPTIONAL)
+	    'dynamic_clauses/dynamic_clauses_rt'
+ 	  ]
         ].
 
 % Should not be used, so we do not document them
@@ -92,55 +97,56 @@ docstr_basiclang :=
 % ---------------------------------------------------------------------------
 
 docstr_compatprolog :=
-	['iso_doc',
-	 'iso_char',
-	 'iso_misc',
-	 'iso_incomplete',
-	 % (most of them obsolete)
-	 'dec10_io',
-	 'old_database',
-	 'ttyout',
-	 'runtime_ops/runtime_ops_doc',
-	 'listing'].
+	'CompatProlog'-[
+          'iso_doc',
+	  'iso_char',
+	  'iso_misc',
+	  'iso_incomplete',
+	  % (most of them obsolete)
+	  'dec10_io',
+	  'old_database',
+	  'ttyout',
+	  'runtime_ops/runtime_ops_doc',
+	  'listing'
+        ].
 
 % ---------------------------------------------------------------------------
 
+% (Standard library)
 docstr_stdlibs :=
-	[% (Standard library)
-	 %
-	 'default_predicates',
-	 %
-	 'between',
-	 %
-	 'aggregates',
-	 %
-	 'libpaths',
-	 %
-	 'assertions/assrt_lib',
-	 'compiler/compiler', % TODO: for dynamic compilation and code loading
-	 % Streams and I/O
-	 'streams_basic', % engine
-	 'io_basic', % engine
-	 % Read/write terms
-	 'tokenize',
-	 'read',
-	 'read_from_string',
-	 'write',
-	 'operators',
-	 'format',
-	 % Some useful I/O predicates
-	 'streams_utils',
-	 % Message printing
-	 'io_aux', % engine
-	 % Runtime system (the engine)
-	 'prolog_flags', % engine
-	 'system_info', % engine
-	 'prolog_sys',
-	 % OS
-	 'system',
-	 'pathnames',
-	 'process/process'-['process/process_channel']
-	 %
+	'StdLibs'-[
+	  'default_predicates',
+	  %
+	  'between',
+	  %
+	  'aggregates',
+	  %
+	  'libpaths',
+	  %
+	  'assertions/assrt_lib',
+	  'compiler/compiler', % TODO: for dynamic compilation and code loading
+	  % Streams and I/O
+	  'streams_basic', % engine
+	  'io_basic', % engine
+	  % Read/write terms
+	  'tokenize',
+	  'read',
+	  'read_from_string',
+	  'write',
+	  'operators',
+	  'format',
+	  % Some useful I/O predicates
+	  'streams_utils',
+	  % Message printing
+	  'io_aux', % engine
+	  % Runtime system (the engine)
+	  'prolog_flags', % engine
+	  'system_info', % engine
+	  'prolog_sys',
+	  % OS
+	  'system',
+	  'pathnames',
+	  'process/process'-['process/process_channel']
 	 ].
 
 %    'classic_doc'
@@ -148,184 +154,201 @@ docstr_stdlibs :=
 % ---------------------------------------------------------------------------
 
 docstr_assrtlang :=
-	['assertions/assertions_doc'-[
-	   'assertions/assertions_props'
-         ],
-	 'regtypes/regtypes_doc',
-	 'assertions/native_props',
-	 %
-	 'isomodes/isomodes_doc',
-	 'basicmodes/basicmodes_doc',
-	 %
-	 'doccomments/doccomments_doc'
+	'AssrtLang'-[
+          'assertions/assertions_doc'-[
+	    'assertions/assertions_props'
+          ],
+	  'regtypes/regtypes_doc',
+	  'assertions/native_props',
+	  %
+	  'isomodes/isomodes_doc',
+	  'basicmodes/basicmodes_doc',
+	  %
+	  'doccomments/doccomments_doc'
 	].
 	 
 % ---------------------------------------------------------------------------
 
 docstr_extralibs :=
-	['getopts',
-	 'messages',
-	 %
-	 'streams',
-	 'io_alias_redirection',
-	 %
-	 'port_reify',
-	 'io_port_reify',
-	 %
-	 'ctrlcclean',
-	 'errhandle',
-	 %
-	 'fastrw',
-	 %
-	 'counters',
-	 %
-	 'file_locks/file_locks',
-	 %
-	 'terms',
-	 'terms_check',
-	 'terms_vars',
-	 'cyclic_terms',
-	 %
-	 'modblobs/modblobs',
-	 %
-	 'formulae',
-	 % Pretty-printers
-	 'pretty_print',
-	 'assertions/assrt_write',
-	 %
-	 'syntax_highlight/syntax_highlight',
-	 %
-	 'librowser/librowser',
-	 % Debugging
-	 'tracing/traces'-[
-           'byrdbox/byrd'
-         ],
-	 % Debugging packages
-	 'expansion_tools',
-	 %
-	 'concurrency/concurrency',
-	 'conc_aggregates',
-	 %
-	 'random/random',
-	 %
-	 'random_aggregates',
-	 %
-	 'sockets/sockets',
-	 'sockets/sockets_io',
-         %
-	 'http/http_doc'-[
-	   'http/http_client',
-	   'http/http_server',
-	   'http/url',
-	   'http/http_forms',
-	   'http/cgi',
-	   % (internal)
-	   'http/http_messages',
-	   'http/http_date',
-	   'http/http_grammar'
-	 ],
-	 %
-	 'pillow/pillow_doc'-[
-	   'pillow/html',
-	   'pillow/json',
-	   'pillow/color_space'
-         ],
-	 %
-	 'system_extra',
-	 %
-	 'source_tree/source_tree',
-	 %
-	 'archive_files',
-	 %
-	 'version_strings',
-	 %
-	 'glob',
-	 'regexp/regexp_doc'-['regexp/regexp_code'],
-	 'text_template',
-	 %
-	 'parse_shell_args',
-	 %
-	 'symfnames/symfnames',
-	 %
-	 'menu/menu_doc'-[
-	   'menu/menu_generator'
-         ],
-	 %
-	 'opendoc',
-	 %
-	 'emacs/emacs'
-	 ].
+	'ExtraLibs'-[
+	  'getopts',
+	  'messages',
+	  %
+	  'streams',
+	  'io_alias_redirection',
+	  %
+	  'port_reify',
+	  'io_port_reify',
+	  %
+	  'ctrlcclean',
+	  'errhandle',
+	  %
+	  'fastrw',
+	  %
+	  'counters',
+	  %
+	  'file_locks/file_locks',
+	  %
+	  'terms',
+	  'terms_check',
+	  'terms_vars',
+	  'cyclic_terms',
+	  %
+	  'modblobs/modblobs',
+	  %
+	  'formulae',
+	  % Pretty-printers
+	  'pretty_print',
+	  'assertions/assrt_write',
+	  %
+	  'syntax_highlight/syntax_highlight',
+	  %
+	  'librowser/librowser',
+	  % Debugging
+	  'tracing/traces'-[
+            'byrdbox/byrd'
+          ],
+	  % Debugging packages
+	  'expansion_tools',
+	  %
+	  'concurrency/concurrency',
+	  'conc_aggregates',
+	  %
+	  'random/random',
+	  %
+	  'random_aggregates',
+	  %
+	  'sockets/sockets',
+	  'sockets/sockets_io',
+          %
+	  ~docstr_http,
+	  %
+	  ~docstr_pillow,
+	  %
+	  'system_extra',
+	  %
+	  'source_tree/source_tree',
+	  %
+	  'archive_files',
+	  %
+	  'version_strings',
+	  %
+	  'glob',
+	  'regexp/regexp_doc'-['regexp/regexp_code'],
+	  'text_template',
+	  %
+	  'parse_shell_args',
+	  %
+	  'symfnames/symfnames',
+	  %
+	  'menu/menu_doc'-[
+	    'menu/menu_generator'
+          ],
+	  %
+	  'opendoc',
+	  %
+	  'emacs/emacs'
+	].
+
+docstr_http :=
+	'http/http_doc'-[
+	  'http/http_client',
+	  'http/http_server',
+	  'http/url',
+	  'http/http_forms',
+	  'http/cgi',
+	  % (internal)
+	  'http/http_messages',
+	  'http/http_date',
+	  'http/http_grammar'
+	].
+
+docstr_pillow :=
+	'pillow/pillow_doc'-[
+	  'pillow/html',
+	  'pillow/json',
+	  'pillow/color_space'
+        ].
 
 % ---------------------------------------------------------------------------
 
 docstr_extendlang :=
-	['pure/pure_doc',
-	 % Higher order, interfaces, etc.
-	 'hiord_rt', % engine
-	 'hiordlib',
-	 'traits/traits_doc',
-	 % Records and functional notation
-	 'argnames/argnames_doc',
-	 'fsyntax/fsyntax_doc',
-	 % DCGs % TODO: add implicits/fluids
-	 'dcg/dcg_doc'-['dcg/dcg_phrase_doc'],
-	 % Mutables and implicits (globals)
-	 'global_vars',
-	 'mutables',
-	 %
-	 'odd', % setarg/3, undo/1 % TODO: rename pred
-	 % Control and evaluation strategy
-	 'indexer/indexer_doc',
-	 'block/block_doc',
-	 'freeze/freeze',
-	 'when/when',
-	 'andorra/andorra_doc',
-	 'det_hook/det_hook_doc'-[
-	   'det_hook/det_hook_rt'
-         ],
-	 'lazy/lazy_doc'-[
-	   'lazy/lazy_lib'
-         ],
-	 % TODO: undo/1 should be here
-	 % Concurrency and distributed
-	 'actmod/actmod_doc'-[
-	   'actmod/actmod_dist'-[
-             'actmod/regp_filebased',
-             'actmod/regp_platformbased',
-             'actmod/regp_webbased'
-	   ],
-           'actmod/actmod_process',
-           'actmod/actmod_rt'
-         ],
-	 %
-	 'bf/bf_doc',
-	 'id/id_doc',
-	 % Constrain domains
-	 'clpq/clpq_doc',
-	 'clpr/clpr_doc',
-	 'clpfd/clpfd_doc'-[
-	   'clpfd/clpfd_rt'
-         ],
-         % Persistence
-	 'persdb/persdb_rt'-[
-	   'persdb/persdb_examples',
-	   'persdb/datadir'
-         ],
-	 'factsdb/factsdb_doc'-[
-	   'factsdb/factsdb_rt'
-         ],
-	 % Attributed variables (used by other extensions)
-	 'attr/attr_doc' - [
-           'attr/attr_rt',
-           'attributes' % engine
-         ],
-	 % Foreign interface to C
-	 'foreign_interface/foreign_interface_doc'-[
-	   'foreign_interface/foreign_interface_properties',
-	   'foreign_compilation',
-	   'foreign_interface/build_foreign_interface'
-         ]
-         ].
+	'ExtendLang'-['pure/pure_doc',
+	  % Higher order, interfaces, etc.
+	  'hiord_rt', % engine
+	  'hiordlib',
+	  'traits/traits_doc',
+	  % Records and functional notation
+	  'argnames/argnames_doc',
+	  'fsyntax/fsyntax_doc',
+	  % DCGs % TODO: add implicits/fluids
+	  'dcg/dcg_doc'-['dcg/dcg_phrase_doc'],
+	  % Mutables and implicits (globals)
+	  'global_vars',
+	  'mutables',
+	  %
+	  'odd', % setarg/3, undo/1 % TODO: rename pred
+	  % Control and evaluation strategy
+	  'indexer/indexer_doc',
+	  'block/block_doc',
+	  'freeze/freeze',
+	  'when/when',
+	  'andorra/andorra_doc',
+	  'det_hook/det_hook_doc'-[
+	    'det_hook/det_hook_rt'
+          ],
+	  'lazy/lazy_doc'-[
+	    'lazy/lazy_lib'
+          ],
+	  % TODO: undo/1 should be here
+	  % Concurrency and distributed
+	  ~docstr_actmod,
+	  %
+	  'bf/bf_doc',
+	  'id/id_doc',
+	  % Constrain domains
+	  'clpq/clpq_doc',
+	  'clpr/clpr_doc',
+	  'clpfd/clpfd_doc'-[
+	    'clpfd/clpfd_rt'
+          ],
+          % Persistence
+	  'persdb/persdb_rt'-[
+	    'persdb/persdb_examples',
+	    'persdb/datadir'
+          ],
+	  'factsdb/factsdb_doc'-[
+	    'factsdb/factsdb_rt'
+          ],
+	  % Attributed variables (used by other extensions)
+	  ~docstr_attr,
+	  % Foreign interface to C
+	  ~docstr_foreign
+        ].
+
+docstr_actmod :=
+	'actmod/actmod_doc'-[
+	  'actmod/actmod_dist'-[
+            'actmod/regp_filebased',
+            'actmod/regp_platformbased',
+            'actmod/regp_webbased'
+	  ],
+          'actmod/actmod_process',
+          'actmod/actmod_rt'
+	].
+
+docstr_attr :=
+	'attr/attr_doc'-[
+          'attr/attr_rt',
+          'attributes' % engine
+        ].
+
+docstr_foreign :=
+	'foreign_interface/foreign_interface_doc'-[
+	  'foreign_interface/foreign_interface_properties',
+	  'foreign_compilation',
+	  'foreign_interface/build_foreign_interface'
+        ].
 
 % ---------------------------------------------------------------------------
 
@@ -333,32 +356,33 @@ docstr_extendlang :=
 %   [1] https://xlinux.nist.gov/dads/HTML/dataStructure.html
 %   [2] https://xlinux.nist.gov/dads/HTML/abstractDataType.html
 docstr_datastructuresalgs :=
-	[% Lists and strings
-	 'lists',
-	 'sort',
-	 'llists',
-	 'idlists',
-	 'numlists',
-	 'strings',
-	 'fuzzy_search/fuzzy_search',
-	 'diff',
-	 % Maps
-	 'dict',
-	 'vndict',
-	 'arrays',
-	 'assoc',
-	 'keys', % TODO: document/merge
-	 % Graphs
-	 'graphs/graphs',
-	 'graphs/ugraphs',
-	 % 'graphs/wgraphs', % it was a verbatim copy of lgraphs using X is min(A,B) instead of min/3
-	 'graphs/lgraphs',
-	 % Queues
-	 'queues',
-	 % Sets
-	 'sets',
-	 'bitcodesets', % TODO: review
-	 'lsets' % TODO: document/merge
+	'DataStructuresAlgs'-[
+          % Lists and strings
+	  'lists',
+	  'sort',
+	  'llists',
+	  'idlists',
+	  'numlists',
+	  'strings',
+	  'fuzzy_search/fuzzy_search',
+	  'diff',
+	  % Maps
+	  'dict',
+	  'vndict',
+	  'arrays',
+	  'assoc',
+	  'keys', % TODO: document/merge
+	  % Graphs
+	  'graphs/graphs',
+	  'graphs/ugraphs',
+	  % 'graphs/wgraphs', % it was a verbatim copy of lgraphs using X is min(A,B) instead of min/3
+	  'graphs/lgraphs',
+	  % Queues
+	  'queues',
+	  % Sets
+	  'sets',
+	  'bitcodesets', % TODO: review
+	  'lsets' % TODO: document/merge
         ].
 
 % ---------------------------------------------------------------------------

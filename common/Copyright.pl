e060e47c76459665d06603a3ca28fc60bce9422d
Manuel Hermenegildo <herme@fi.upm.es>
2018-08-09 05:18:55 +0200

    Some improvements in documentation of read_from_string.pl
    Src-commit: 75f720ca8c1a7eb6b1e5320e84ade84e880c432e

--- common/Copyright.pl
+++ common/Copyright.pl
@@ -1,11 +1,9 @@
 
 %% + / or The Free Software Foundation?
-%:- doc(copyright,"Copyright @copyright{} 1997-2011 Francisco Bueno,
-%	Daniel Cabeza, Manuel Carro, Manuel Hermenegildo,
-%	P. L@'{o}pez, and Germ@'{a}n Puebla
-%	@include{DocCopyright.lpdoc}").
+%% This is the Open Resources Magazine License (disappeared?)
+%% Looks based on the free documentation dissemination license
+%% Say Except where otherwise stated?
 :- doc(copyright,"Copyright @copyright{} 1997-2011 Francisco Bueno,
-   Manuel Carro, Remy Haemmerl@'{e}, Manuel Hermenegildo,
-   Pedro L@'{o}pez, Edison Mera,
-   Jos@'{e} F. Morales, and Germ@'{a}n Puebla
-   @include{DocCopyright.lpdoc}").
+	Daniel Cabeza, Manuel Carro, Manuel Hermenegildo,
+	P. L@'{o}pez, and Germ@'{a}n Puebla
+	@include{DocCopyright.lpdoc}").

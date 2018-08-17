2250f65e618452fab674416818d4ff8714a8df54
Manuel Hermenegildo <herme@fi.upm.es>
2018-08-17 16:25:55 +0200

    Minor in developers while reading
    Src-commit: ddbfcca739371a3dabafc86dc134e02c96420aa7

--- developers/CiaoDevel.pl
+++ developers/CiaoDevel.pl
@@ -1,16 +1,15 @@
-%:- module(_, [], []).
 :- use_package(doccomments).
 :- doc(filetype, application). % TODO: or 'documentation'?
 
-:- doc(title, "Developers and Contributors Guide").
+%! title:            Developers and Contributors Guide
+%
+%  subtitle_extra:   GUIDE
+%  subtitle_extra:   __The Ciao Documentation Series__
+%  subtitle_extra:   [[https://ciao-lang.org/]]
+%  subtitle_extra:   _Generated/Printed on:_ @today{}
+%  subtitle_extra:   Technical Report CLIP 3/97-@version{}
 
 :- doc(logo, 'ciao-grimoire-128h').
-
-:- doc(subtitle_extra,"REFERENCE MANUAL").
-:- doc(subtitle_extra,"@bf{The Ciao Documentation Series}").
-:- doc(subtitle_extra,"@href{https://ciao-lang.org/}").
-:- doc(subtitle_extra,"@em{Generated/Printed on:} @today{}").
-:- doc(subtitle_extra,"Technical Report CLIP 3/97-@version{}").
 
 % TODO: This file is not using the special acknowledgments section.
 % TODO: Replace 'credits' by 'editor'? (JFMC)
@@ -21,12 +20,15 @@
 %% :- doc(bug,"Although the documentation is acceptable at this
 %%    point, we are still really in beta mode in this regard.").
 
-%! \module
-%    This is the Ciao developers and contributors handbook.
+%! module:
+%    This is an informal Ciao developers and contributors handbook. 
 %
-%    It covers some topics the developers and contributors should be
-%    aware of: coding style, code reviews, testing, and preparing
-%    releases and distributions.
+%    It covers some topics that developers and contributors should be
+%    aware of: coding style, code reviews, testing, preparing
+%    releases and distributions, etc.
+% 
+%    This is (permanently?) work in progress, i.e., different parts
+%    are in different states of maturity.
 %
 %    If you with to contribute please contact us at
 %    \email{ciao\@cliplab.org}.

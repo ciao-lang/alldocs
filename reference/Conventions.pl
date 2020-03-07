d506e949ddb4788afd1e2ba264a4d1599bfca79b
Manuel Hermenegildo <manuel.hermenegildo@imdea.org>
2020-03-07 14:39:09 +0100

    Missing file
    Src-commit: d563a4ef6962f86439dd5fc9b611cc36383ec39d

---
+++ reference/Conventions.pl
@@ -0,0 +1,9 @@
+:- use_package(assertions).
+
+:- doc(filetype, documentation).
+
+:- doc(title,"Basic concepts and conventions").
+
+:- doc(author, "The Ciao Development Team").
+
+:- doc(module,"@include{Conventions.lpdoc}"). 
